09f1782c125939281c15643195e6f6fae15c59e5
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-07-30 15:37:03 +0100

    Database setup

--- go_give/__init__.py
+++ go_give/__init__.py
@@ -1,17 +1,37 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    if 'RDS_HOSTNAME' in os.environ:
+      DATABASE = {
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+      }
+      database_url = 'mysql://%(USER)s:%(PASSWORD)s@%(HOST)s:%(PORT)s/%(NAME)s' % DATABASE
+    else:
+      database_url = 'sqlite:///' + os.path.join(app.instance_path, 'go_give.sqlite')
+
+    app.config.from_mapping(
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+        SQLALCHEMY_DATABASE_URI = database_url,
+        SQLALCHEMY_POOL_RECYCLE = 280,
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
